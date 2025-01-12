eb62030c626b38dad6fe724ff979d12fc3e46b54
tanishkaraj <160894813+tanishkaraj@users.noreply.github.com>
2025-01-12 21:42:42 +0530

    Update user.js

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -10,6 +10,6 @@ const userSchema = new mongoose.Schema({
   },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-}, { timestamps: true });
+}, { timestamps: true }); //optional to store change timestamp of user
 
 module.exports = mongoose.model('User', userSchema);
