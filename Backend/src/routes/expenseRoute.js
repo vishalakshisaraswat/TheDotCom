b52475d5ba81858f5f4e53f0420b9add7f9c65ac
Srishty01 <141113920+Srishty01@users.noreply.github.com>
2025-01-26 15:58:27 +0530

    Update expenseRoute.js

--- Backend/src/routes/expenseRoute.js
+++ Backend/src/routes/expenseRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-const Expense = require("../models/expense");
+const Expense = require('../models/expense');
 const auth = require("../middleware/auth");
 
 // Create Expense
